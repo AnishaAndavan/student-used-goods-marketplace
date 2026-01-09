e3a553f1235c962cdf990f3572516061165d8ff9
AnishaAndavan <2212082@nec.edu.in>
2026-01-09 09:38:47 +0530

    Prepare backend for Render deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://student-used-goods-marketplace.onrender.com/",
+  baseURL: "https://student-used-goods-marketplace.onrender.com/api",
 });
 
 // attach token automatically
